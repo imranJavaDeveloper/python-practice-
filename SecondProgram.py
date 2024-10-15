79d14c03b7e7d038d256424d721ac5ab8c8f01b2
Md Imran Alam <mdimranalam2348@gmail.com>
2024-10-15 23:54:45 +0530

    how take input in the python language

---
+++ SecondProgram.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+firstName = input("Enter your first name: ")
+
+middleName=input("Enter your middle name: ")
+
+lastName=input("Enter your Last name")
+
+print("Hello "+firstName+" "+middleName+" "+lastName)
+
