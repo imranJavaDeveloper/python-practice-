953fd2c4a2d445e6d244b79ce1a35af3e3e15299
Md Imran Alam <mdimranalam2348@gmail.com>
2024-10-15 23:44:21 +0530

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,8 @@
+# Hello World program
+print("Hello, World!")
+
+print("I am learning python from apna college")
+print("python its esay as compare to java")
+print(" I love python its very simple ")
+
+print("thank you")
